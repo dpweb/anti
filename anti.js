b471af584bec63cc413f5ad01e1eef7eb8ce466a
C Borkert <success@dpsw.info>
2014-01-18 15:41:04 -0600

    fix

--- anti.js
+++ anti.js
@@ -1,4 +1,13 @@
 function anti(o, fn, t){
+	if(o && o.dir){
+		var arr = [];
+		require('fs').readdirSync(o.dir).map(function(fn){
+			var path = './' + o.dir + fn;
+			var data = require(path);
+			arr.push(anti(data, path, t));
+		})
+	}
+
 	var isnode = typeof window==='undefined';
 	var save = function(){
 		var f = isnode ? require('fs').writeFileSync : localStorage.setItem.bind(localStorage);
